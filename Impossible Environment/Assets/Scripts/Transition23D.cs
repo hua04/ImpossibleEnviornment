421c64a56dd0ef85f0859eb031d516d2d2db8042
Hua <hua.caparusso@gmail.com>
2023-04-28 11:16:08 -0400

    Change from 3D to 2D scene

---
+++ Impossible Environment/Assets/Scripts/Transition23D.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Transition23D : MonoBehaviour
+{
+    public Collider player;
+    public GameObject instructionsIn;
+    public GameObject instructionsInteract;
+    public GameObject instructionsOut;
+    public GameObject twoDCam;
+
+    public void OnTriggerStay(Collider player)
+    {
+        instructionsIn.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            instructionsIn.SetActive(false);
+            instructionsInteract.SetActive(true);
+            instructionsOut.SetActive(true);
+            twoDCam.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    public void OnTriggerExit(Collider player)
+    {
+        instructionsIn.SetActive(false);
+    }
+    
+        public void Update()
+    {
+        if (twoDCam.activeSelf == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Cursor.visible = false;
+                instructionsIn.SetActive(false);
+                instructionsInteract.SetActive(false);
+                instructionsOut.SetActive(false);
+                twoDCam.SetActive(false);
+            }
+        }
+
+    }
+
+}
