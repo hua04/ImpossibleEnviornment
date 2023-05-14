26956f3f2220dd33218d47893746c81385f6b0a4
Hua <hua.caparusso@gmail.com>
2023-05-14 02:26:09 -0400

    Added sound, dialogue, ending, etc.

---
+++ Impossible Environment/Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public Animator animator;
+
+    public void FadeRestart()
+    {
+        animator.SetTrigger("Death");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
+
--- Impossible Environment/Assets/Scripts/shufflePosition.cs
+++ Impossible Environment/Assets/Scripts/shufflePosition.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class shufflePosition : MonoBehaviour
